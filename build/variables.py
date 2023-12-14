efee9bd04a9979f3cdc36f450a94252e2bb19bbc
Fundgod <fundgod.savin@gmail.com>
2023-12-14 03:55:06 +0100

    Deleted one folder in path

--- build/variables.py
+++ build/variables.py
@@ -4,7 +4,7 @@ table_columns = ["ID", "Title", "Author", "Year", "Rate"]
 table_columns_sizes = [100, 300, 300, 200, 75]
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r".\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r".\assets")
 
 
 def relative_to_assets(path: str) -> Path:
